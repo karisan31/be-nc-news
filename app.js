601690932c6d14d2ad7a03fc7dcd17192ac770dd
karisan31 <kmathan31@outlook.com>
2024-01-15 14:27:44 +0000

    Add Task 2-get-api-topics

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const { getTopics } = require("./controllers/topics-controller")
 
 const app = express();
 
+app.use(express.json());
+
+
 app.get("/api/topics", getTopics);
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    if (err.msg === 'Route Not Found') {
+        res.status(404).send({ msg: 'Route Not Found' })
+    }
+    else {
+        next(err)
+    }
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ msg: "Internal Server Error" })
+})
 
 module.exports = app;
