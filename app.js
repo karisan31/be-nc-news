775990b3d455fae8e32afee88516eec00e79c6f2
karisan31 <kmathan31@outlook.com>
2024-01-16 12:01:34 +0000

    Add task 5-get-api-articles

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics-controller")
 const { getEndpoints } = require("./controllers/endpoints-controller")
-const { getArticleById } = require("./controllers/articles-controller")
+const { getArticleById, getArticles } = require("./controllers/articles-controller")
 
 const app = express();
 
 app.get("/api", getEndpoints)
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/articles", getArticles)
 
 app.use((err, req, res, next) => {
     if (err.code === '22P02') {
--- models/articles-model.js
+++ models/articles-model.js
@@ -9,3 +9,21 @@ module.exports.fetchArticleById = (article_id) => {
             return rows[0];
         })
 }
+
+module.exports.fetchArticles = () => {
+    
+    const queryStr = `
+        SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
+        COUNT(comments.comment_id) AS comment_count
+        FROM articles
+        LEFT JOIN comments ON articles.article_id = comments.article_id
+        GROUP BY articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url
+        ORDER BY created_at desc
+    `
+    
+    return db.query(queryStr)
+        .then(({ rows }) => {
+            console.log(rows)
+            return rows;
+        })
+}
