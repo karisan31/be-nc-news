3d3eb32710891ffbcf4ca1143ea96e05bc56cbc3
karisan31 <kmathan31@outlook.com>
2024-01-17 20:59:25 +0000

    Add Task 9-delete-api-comments-by-comment-id (removed one console.log)

--- models/comments-model.js
+++ models/comments-model.js
@@ -3,7 +3,6 @@ const db = require("../db/connection")
 exports.removeCommentById = (comment_id) => {
     return db.query('DELETE FROM comments WHERE comment_id = $1;', [comment_id])
       .then((result) => {
-        console.log(result)
         if(result.rowCount === 0) {
           return Promise.reject({ msg: "Comment Does Not Exist"})
         }
