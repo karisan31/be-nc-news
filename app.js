3d7c08d5088b50c01c77e6987028d978d88ae1c5
karisan31 <kmathan31@outlook.com>
2024-02-04 23:40:38 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
+const cors = require("cors");
 const express = require("express");
 const { getTopics } = require("./controllers/topics-controller");
 const { getEndpoints } = require("./controllers/endpoints-controller");
 const {
-    getArticleById,
-    getArticles,
-    getCommentsByArticleId,
-    postCommentByArticleId,
-    patchArticleById,
+  getArticleById,
+  getArticles,
+  getCommentsByArticleId,
+  postCommentByArticleId,
+  patchArticleById,
 } = require("./controllers/articles-controller");
 const { deleteCommentById } = require("./controllers/comments-controller");
 const { getUsers } = require("./controllers/users-controller");
 
 const {
-    handleCustomErrors,
-    handlePsqlErrors,
-    handleServerErrors,
+  handleCustomErrors,
+  handlePsqlErrors,
+  handleServerErrors,
 } = require("./errors/index");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/api", getEndpoints);
@@ -31,7 +33,7 @@ app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 app.patch("/api/articles/:article_id", patchArticleById);
 app.delete("/api/comments/:comment_id", deleteCommentById);
 
-app.get("/api/users", getUsers)
+app.get("/api/users", getUsers);
 
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
